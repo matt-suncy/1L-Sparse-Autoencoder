bbad45e5e6d805f544c1c920c81b0a1943d67ce1
matt_suncy <matt.suncy@gmail.com>
2024-06-21 16:50:37 +0000

    Start of implementing feature steering

---
+++ feature_steering.py
@@ -0,0 +1,31 @@
+import transformer_lens
+from transformer_lens import HookedTransformer, utils
+import torch
+import numpy as np
+import gradio as gr
+import pprint
+import json
+import torch.nn as nn
+import torch.nn.functional as F
+from datasets import load_dataset
+from huggingface_hub import HfApi
+from IPython.display import HTML
+from functools import partial
+import tqdm.notebook as tqdm
+import plotly.express as px
+import pandas as pd
+
+
+model = HookedTransformer.from_pretrained("gpt2").to(torch.float32)
+print('type(model)', type(model))
+n_layers = model.cfg.n_layers
+d_model = model.cfg.d_model
+n_heads = model.cfg.n_heads
+d_head = model.cfg.d_head
+d_mlp = model.cfg.d_mlp
+d_vocab = model.cfg.d_vocab
+
+
+prompt = "Why did the chicken cross the"
+answer = "road"
+utils.test_prompt(prompt, answer, model)
